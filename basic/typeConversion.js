702b3d912249ab62ee6bf59b74ef8592d73e3f43
Ajeet Jaiswal <43951259+ajeetjaiswal@users.noreply.github.com>
2024-02-16 17:48:58 +0000

    Type Conversion

---
+++ basic/typeConversion.js
@@ -0,0 +1,38 @@
+// let score ="Ajeet"
+// console.log(typeof score )
+// console.log(typeof (score) )
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//let isLoggedIn ="Ajeet"
+// let isLoggedIn =""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// let someNumber = 123
+
+// let valueInString= String(someNumber)
+
+// console.log(someNumber)
+// console.log(typeof stringNumber)
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+let str1 ="hello"
+let str2 ="Ajeet"
+
+let str3 =str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log("1"+2+2)
+
+let gameCounter =100
+++gameCounter
+console.log(gameCounter)
