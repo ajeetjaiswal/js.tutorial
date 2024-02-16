9c262ba7892120135a93f9cfd8b3dbcc8e81425d
Ajeet Jaiswal <43951259+ajeetjaiswal@users.noreply.github.com>
2024-02-16 17:09:45 +0000

    container config done

---
+++ basic/variable.js
@@ -0,0 +1,9 @@
+const accountId = 2255 //recomended
+var accountEmail = "abc@gmail.com" //not recomended to use
+let accountAddress = "bagaha"//recomended
+accountPassword= "12345" //not allowed 
+let accountPremium    //no value so it print undefined
+
+//console.log(accountId)
+
+console.table([accountId,accountEmail,accountAddress,accountPassword,accountPremium]) // console.table use to print all the value in a single table 
