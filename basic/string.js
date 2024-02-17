74d4643a6e8d606f0aed4d5669d715a00d5ec4a6
Ajeet Jaiswal <43951259+ajeetjaiswal@users.noreply.github.com>
2024-02-17 16:18:59 +0000

    string

---
+++ basic/string.js
@@ -0,0 +1,8 @@
+ const string1= "A string primitive";
+ const string2= 'Also a sting primitive';
+ const string3= `yet another string primitive`;
+
+ console.log(sting1);
+
+ let a="ajeet"
+ console.log(a);
